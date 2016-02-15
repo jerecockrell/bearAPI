f21df2cdb6812784eb2d849001ab0d084c1f1170
jerecockrell <jere.cockrell@gmail.com>
2016-02-15 10:25:30 -0700

    add bearAPI calculator files

---
+++ myCalculator.js
@@ -0,0 +1,9 @@
+//IMPORT THE OTHER FILE
+var Calculator = require('./calculator')
+
+//CREATE A NEW CALCULATOR
+var myCalculator = new  Calculator();
+console.log(myCalculator);
+
+//TEST THE SUM FUNCTION
+
