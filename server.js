32e6ceaa827d293d45a092574c6f859ce50662ae
jerecockrell <jere.cockrell@gmail.com>
2016-02-15 14:00:43 -0700

    add new bear data field

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser());
 
 var monte = {name: 'monte', id: 0, age: 50, gender: 'male'};
 var paul = {name: 'paul', id: 1, age: 30, gender: 'male'};
-var mary = {name: 'mary', id: 2, age: 40, gender: 'female'}
+var mary = {name: 'mary', id: 2, age: 40, gender: 'female'};
 
-var bears = [monte, paul, mary]
+var bears = [monte, paul, mary];
 
 app.get('/api/bears', function(req, res){
-	res.json(bears)
+	res.json(bears);
+});
+
+app.post('/api/bear', function(req, res){
+	var newBear = {};
+
+	console.log('about to create a new bear');
+
+	newBear.name = req.body.name;
+	newBear.age = req.body.age;
+	newBear.gender = req.body.gender;
+	newBear.id = new Date().getTime();
+
+	console.log('pushing: ', newBear);
+
+	bears.push(newBear);
+
+	res.json(bears);
 });
 
 app.listen(3000, function(){
-	console.log('the server is up and running..')
+	console.log('the server is up and running..');
 });  
